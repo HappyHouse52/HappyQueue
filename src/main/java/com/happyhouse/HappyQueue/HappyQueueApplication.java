8bab5b7682ca52e332a50823463dde316c422de7
Dylan Bannon <dbannon@spotify.com>
2020-02-03 17:34:09 -0500

    wire up the spotify API

--- src/main/java/com/happyhouse/HappyQueue/HappyQueueApplication.java
+++ src/main/java/com/happyhouse/HappyQueue/HappyQueueApplication.java
@@ -1,10 +1,40 @@
 package com.happyhouse.HappyQueue;
 
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
+import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class HappyQueueApplication {
+	@Value("${SPOTIFY_CLIENT_ID}")
+	private String clientId;
+	@Value("${SPOTIFY_CLIENT_SECRET}")
+	private String clientSecret;
+
+	@Bean
+	public SpotifyApi spotifyApi() {
+
+		final SpotifyApi spotifyApi = SpotifyApi.builder()
+				.setClientId(clientId)
+				.setClientSecret(clientSecret)
+				.build();
+		final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials().build();
+		try {
+			final ClientCredentials credentials = clientCredentialsRequest.execute();
+			spotifyApi.setAccessToken(credentials.getAccessToken());
+			return spotifyApi;
+		} catch (IOException | SpotifyWebApiException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HappyQueueApplication.class, args);
 	}
--- src/main/java/com/happyhouse/HappyQueue/controllers/QueueController.java
+++ src/main/java/com/happyhouse/HappyQueue/controllers/QueueController.java
@@ -1,14 +1,45 @@
 package com.happyhouse.HappyQueue.controllers;
 
 import com.happyhouse.HappyQueue.model.SpotifyQueue;
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.model_objects.specification.ArtistSimplified;
+import com.wrapper.spotify.model_objects.specification.Image;
+import com.wrapper.spotify.model_objects.specification.Track;
+import com.wrapper.spotify.requests.data.search.simplified.SearchTracksRequest;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class QueueController {
+  private final SpotifyApi spotifyApi;
+
+  public QueueController(final SpotifyApi spotifyApi) {this.spotifyApi = spotifyApi;}
+
   @GetMapping("/v1/queues")
   public SpotifyQueue getQueue(@RequestParam(name = "id", defaultValue = "test") String queueId) {
-    return SpotifyQueue.builder().setId(queueId).build();
+    final SearchTracksRequest searchRequest = spotifyApi.searchTracks(queueId).build();
+    final List<Track> results;
+    try {
+      results = Arrays.asList(searchRequest.execute().getItems());
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    return SpotifyQueue.builder()
+        .setId(queueId)
+        .setTracks(results.stream().map(this::fromSpotifyModel).collect(Collectors.toList()))
+        .build();
+  }
+
+  private com.happyhouse.HappyQueue.model.Track fromSpotifyModel(Track track) {
+    return com.happyhouse.HappyQueue.model.Track.builder()
+        .setName(track.getName())
+        .setImageUrl(Stream.of(track.getAlbum().getImages()).map(Image::getUrl).findFirst().orElseThrow())
+        .setSubtitle(Stream.of(track.getArtists()).map(ArtistSimplified::getName).collect(Collectors.joining(", ")))
+        .build();
   }
 }
