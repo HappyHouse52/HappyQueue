0c64d4e53d50d026ede83ccb523efa218e68e6f2
Dylan Bannon <dbannon@spotify.com>
2020-02-03 14:24:45 -0500

    initial commit

---
+++ src/main/java/com/happyhouse/HappyQueue/HappyQueueApplication.java
@@ -0,0 +1,13 @@
+package com.happyhouse.HappyQueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/happyhouse/HappyQueue/HappyQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happyhouse.HappyQueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
