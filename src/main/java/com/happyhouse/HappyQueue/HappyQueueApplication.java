cd6334061e4860cc98175c2a1b0847947f7f8e8d
Dylan Bannon <dbannon@spotify.com>
2020-02-07 12:06:43 -0500

    build spotify API for each call

--- src/main/java/com/happyhouse/HappyQueue/HappyQueueApplication.java
+++ src/main/java/com/happyhouse/HappyQueue/HappyQueueApplication.java
@@ -5,49 +5,21 @@ import com.happyhouse.HappyQueue.model.TrackDb;
 import com.happyhouse.HappyQueue.model.UserDb;
 import com.happyhouse.HappyQueue.repositories.QueueRepository;
 import com.happyhouse.HappyQueue.repositories.UserRepository;
-import com.wrapper.spotify.SpotifyApi;
-import com.wrapper.spotify.exceptions.SpotifyWebApiException;
-import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
-import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
-import java.io.IOException;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class HappyQueueApplication implements CommandLineRunner {
-	@Value("${spotify.client.id}")
-	private String clientId;
-	@Value("${spotify.client.secret}")
-	private String clientSecret;
-
 	private final QueueRepository queueRepository;
 	private final UserRepository userRepository;
 
 	public HappyQueueApplication(QueueRepository queueRepository, UserRepository userRepository) {
 		this.queueRepository = queueRepository;
 		this.userRepository = userRepository;
-	}
-
-	@Bean
-	public SpotifyApi spotifyApi() {
-		final SpotifyApi spotifyApi = SpotifyApi.builder()
-				.setClientId(clientId)
-				.setClientSecret(clientSecret)
-				.build();
-		final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials().build();
-		try {
-			final ClientCredentials credentials = clientCredentialsRequest.execute();
-			spotifyApi.setAccessToken(credentials.getAccessToken());
-			return spotifyApi;
-		} catch (IOException | SpotifyWebApiException e) {
-			throw new RuntimeException(e);
-		}
 	}
 
 	public static void main(String[] args) {
--- src/main/java/com/happyhouse/HappyQueue/controllers/SearchController.java
+++ src/main/java/com/happyhouse/HappyQueue/controllers/SearchController.java
@@ -3,27 +3,35 @@ package com.happyhouse.HappyQueue.controllers;
 import com.happyhouse.HappyQueue.model.SearchResponse;
 import com.happyhouse.HappyQueue.model.Track;
 import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
 import com.wrapper.spotify.model_objects.specification.ArtistSimplified;
 import com.wrapper.spotify.model_objects.specification.Image;
+import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
 import com.wrapper.spotify.requests.data.search.simplified.SearchTracksRequest;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SearchController {
-  private SpotifyApi spotifyApi;
-
-  public SearchController(SpotifyApi spotifyApi) {
-    this.spotifyApi = spotifyApi;
-  }
+  private static final Logger LOG = LoggerFactory.getLogger(SearchController.class);
+  @Value("${spotify.client.id}")
+  private String clientId;
+  @Value("${spotify.client.secret}")
+  private String clientSecret;
 
   @GetMapping(path = "/v1/search")
   public SearchResponse search(@RequestParam(name = "q") String query) {
+    final SpotifyApi spotifyApi = getSpotifyApi();
     final SearchTracksRequest searchRequest = spotifyApi.searchTracks(query).build();
     final List<com.wrapper.spotify.model_objects.specification.Track> results;
     try {
@@ -45,5 +53,21 @@ public class SearchController {
             .collect(Collectors.joining(", ")))
         .setSpotifyUri(track.getUri())
         .build();
+  }
+
+  private SpotifyApi getSpotifyApi() {
+    LOG.info("Building Spotify API");
+    final SpotifyApi spotifyApi = SpotifyApi.builder()
+        .setClientId(clientId)
+        .setClientSecret(clientSecret)
+        .build();
+    final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials().build();
+    try {
+      final ClientCredentials credentials = clientCredentialsRequest.execute();
+      spotifyApi.setAccessToken(credentials.getAccessToken());
+      return spotifyApi;
+    } catch (IOException | SpotifyWebApiException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
