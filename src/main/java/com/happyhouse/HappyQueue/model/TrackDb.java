54b6205f9d65b6f32df41f8862c4b477f0780256
Dylan Bannon <dylanrb123@gmail.com>
2020-02-17 15:52:19 -0500

    imageUri -> imageUrl

--- src/main/java/com/happyhouse/HappyQueue/model/TrackDb.java
+++ src/main/java/com/happyhouse/HappyQueue/model/TrackDb.java
@@ -3,17 +3,12 @@ package com.happyhouse.HappyQueue.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.happyhouse.HappyQueue.model.QueueDb;
-import java.sql.Timestamp;
-import java.time.Instant;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import lombok.Data;
 import lombok.ToString;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
 
 @Data
 @Entity
@@ -25,7 +20,7 @@ public class TrackDb {
   private Integer id;
   private String title;
   private String subtitle;
-  private String imageUri;
+  private String imageUrl;
   private Timestamp queueTime;
   private String spotifyUri;
   private String queuer;
@@ -36,15 +31,15 @@ public class TrackDb {
   @ToString.Exclude
   private QueueDb queue;
 
-  public TrackDb(String title, String subtitle, String imageUri, String spotifyUri, String queuer, QueueDb queue) {
-    this(title, subtitle, imageUri, spotifyUri, queuer);
+  public TrackDb(String title, String subtitle, String imageUrl, String spotifyUri, String queuer, QueueDb queue) {
+    this(title, subtitle, imageUrl, spotifyUri, queuer);
     this.queue = queue;
   }
 
-  public TrackDb(String title, String subtitle, String imageUri, String spotifyUri, String queuer) {
+  public TrackDb(String title, String subtitle, String imageUrl, String spotifyUri, String queuer) {
     this.title = title;
     this.subtitle = subtitle;
-    this.imageUri = imageUri;
+    this.imageUrl = imageUrl;
     this.queueTime = Timestamp.from(Instant.now());
     this.spotifyUri = spotifyUri;
     this.queuer = queuer;
